40bfefbc130355aa52a97f1bbfa52b2310f50cfe
Martin <pruebasfactura123@gmail.com>
2026-06-09 08:54:50 -0700

    Ignorar capas de datos temporalmente

--- src/main/java/com/example/sistema/SistemaApplication.java
+++ src/main/java/com/example/sistema/SistemaApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.sistema"}) // ESTO FUERZA A JAVA A BUSCAR TUS CARPETAS
+@ComponentScan(
+    basePackages = {"com.example.sistema"},
+    excludeFilters = @ComponentScan.Filter(type = org.springframework.context.annotation.FilterType.REGEX, pattern = "com.example.sistema\\.(controller|service|repository)\\..*")
+)
 public class SistemaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SistemaApplication.class, args);
     }
-}       
+}
